4ee135031b840a3b866d8429f2ed2e1f211cacaa
Martin H. J. Bramwell <mhb.warehouseman@gmail.com>
2015-05-31 14:06:35 -0400

    Add first API interaction  Changes to be committed: 	modified:   tests/meteor-node-trello-tests.js

--- tests/meteor-node-trello-tests.js
+++ tests/meteor-node-trello-tests.js
@@ -27,6 +27,12 @@ if (Meteor.isServer) {
   Tinytest.add('trello:have_token_C', function (test) {
     test.equal(process.env.TRELLO_TOKEN_C.length, 64);
   });
+ 
+  Tinytest.add('trello:user_A_named', function (test) {
+    var data = proxyTrello.get("/1/members/me");
+    test.equal(data.username, process.env.TRELLO_USER_A);
+  });
+ 
 
 
 }
